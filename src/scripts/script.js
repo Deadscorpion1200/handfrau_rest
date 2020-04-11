9df5695b6ce172fdbaf210f559297b2a21f7118b
Alexey Kurp <kurp96@ya.ru>
2020-04-11 20:15:45 +0300

    Этап верстки №3

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,5 @@
 let slider = $('.range');
 let fill = $('.bar .fill');
-let square;
-let value = $('.calc-calculator-square__square_count');
-console.log(value.html());
 function setBar()
 {
   fill.css('width', slider.val()+'%')
@@ -11,3 +8,40 @@ function setBar()
 
 slider.on('input', setBar);
 setBar();
+
+$('.navbar-mobile__menu').on('click', function(e)
+{
+  e.preventDefault();
+  $(this).toggleClass('navbar-mobile__menu_active');
+  $('.navbar-nav__mobile').toggleClass('navbar-nav__mobile_active')
+})
+
+let swiper1 = new Swiper('.special-gallery',{
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.special-gallery__pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320:
+    {
+      slidesPerView: 1
+    },
+    576:
+    {
+      slidesPerView: 2,
+    }
+  }
+});
+// let swiper2 = new Swiper('.dirt-slider-wrap',{
+//   direction: 'vertical',
+//   loop: true,
+//   slidesPerView: 1,
+//   centeredSlides: true,
+// })
