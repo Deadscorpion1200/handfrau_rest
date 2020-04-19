d94e00079f82a9b247590c7ebf1afc8c14311276
Alexey Kurp <kurp96@ya.ru>
2020-04-19 11:36:57 +0300

    Верстка блоков 2

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -39,9 +39,103 @@ let swiper1 = new Swiper('.special-gallery',{
     }
   }
 });
-// let swiper2 = new Swiper('.dirt-slider-wrap',{
-//   direction: 'vertical',
-//   loop: true,
-//   slidesPerView: 1,
-//   centeredSlides: true,
-// })
+
+
+let swiper2 = new Swiper ('.command-slider-container', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 3,
+  // If we need pagination
+  pagination: {
+    clickable: true,
+    el: '.command-pagination',
+  },
+  
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.command-button-next',
+    prevEl: '.command-button-prev',
+  },
+
+  breakpoints: {
+    320:
+    {
+      slidesPerView: 1
+    },
+    768:
+    {
+      slidesPerView: 2,
+      centredSlides: false
+    },
+    991:
+    {
+      slidesPerview: 3,
+      centeredSlides: true,
+    }
+  }
+});
+
+let swiper3 = new Swiper('.materials-slider',{
+  loop: true,
+  slidesPerView: 2,
+  navigation: {
+    nextEl: '.materials-button-next',
+    prevEl: '.materials-button-prev',
+  },
+  pagination: {
+    clickable: true,
+    el: '.materials-slider-pagination',
+  }
+  
+});
+let swiper4 = new Swiper('.partners-slider-container',
+{
+  loop: true,
+  navigation: {
+    nextEl: '.partners-button-next',
+    prevEl: '.partners-button-prev'
+  },
+  breakpoints: {
+    1200:
+    {
+      slidesPerView: 4,
+    },
+    991:
+    {
+      slidesPerView: 3
+    },
+    768:
+    {
+      spaceBetween: 90,
+      slidesPerView: 2
+    },
+    320:
+    {
+      splidesPerView: 1,
+      pagination: {
+        clickable: true,
+        el: '.partners-pagination',
+      }
+    }
+  }
+});
+let swiper5 = new Swiper('.reviews-slider-container',
+{
+  loop: true,
+  navigation: {
+    nextEl: '.reviews-button-next',
+    prevEl: '.reviews-button-prev'
+  },
+  pagination: {
+    clickable: true,
+    el: '.reviews-pagination',
+  },
+  breakpoints: {
+    320:
+    {
+      slidesPerView: 1
+    }
+  }
+})
