268e17f9c9324dadd6a9410019dda25f442eb332
Izripov-Yusuf <izripov1998@gmail.com>
2020-04-20 16:04:31 +0300

    Теперь калькулятор отображает количество людей и часов в зависимости от площади

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -28,73 +28,73 @@ services.addEventListener('click', (event) => {
 let inputRange = document.querySelector('.range'),
     area = document.querySelector('.calc-calculator-square__square_count'),
     result = document.querySelector('.calc-result-sum__count'),
-    resultPersonal = document.querySelector('.calc-result-personal__man'),
-    resultHours = document.querySelector('.calc-result-personal__hour');
-    console.log('resultPersonal: ', resultPersonal.textContent);
-    console.log('resultHours: ', resultHours.textContent);
+    resultPersonal = document.querySelectorAll('.calc-result-personal__man'),
+    resultHours = document.querySelectorAll('.calc-result-personal__hour');
+    console.log('resultPersonal: ', resultPersonal);
+    console.log('resultHours: ', resultHours);
 
 inputRange.addEventListener('input', (event) => {
   inputRange.value = event.target.value;
   area.textContent = +inputRange.value;
+  console.log(typeof +inputRange.value);
 
   // Количество людей и часов
-  if (+inputRange.value === 10) {
-    console.log(1);
-    resultPersonal.textContent = '1 человек';
-    resultHours.textContent = '2 часа';
-  } else if (+inputRange.value === 20) {
-    console.log(2);
-    resultPersonal.textContent = '1 человек';
-    resultHours.textContent = '4 часа';
-  } else if (+inputRange.value === 30) {
-    console.log(3);
-    resultPersonal.textContent = '1 человек';
-    resultHours.textContent = '6 часов';
-  } else if (+inputRange.value === 40) {
-    resultPersonal.textContent = '1 человек';
-    resultHours.textContent = '8 часов';
-  } else if (+inputRange.value === 50) {
-    resultPersonal.textContent = '2 человека';
-    resultHours.textContent = '5 часов';
-  } else if (+inputRange.value === 60) {
-    resultPersonal.textContent = '2 человека';
-    resultHours.textContent = '6 часов';
-  } else if (+inputRange.value === 70) {
-    resultPersonal.textContent = '2 человека';
-    resultHours.textContent = '7 часов';
-  } else if (+inputRange.value === 80) {
-    resultPersonal.textContent = '2 человека';
-    resultHours.textContent = '8 часов';
-  } else if (+inputRange.value === 90) {
-    resultPersonal.textContent = '3 человека';
-    resultHours.textContent = '6 часов';
-  } else if (+inputRange.value === 100) {
-    resultPersonal.textContent = '3 человека';
-    resultHours.textContent = '7 часов';
-  } else if (+inputRange.value === 110) {
-    resultPersonal.textContent = '3 человека';
-    resultHours.textContent = '8 часов';
-  } else if (+inputRange.value === 120) {
-    resultPersonal.textContent = '3 человека';
-    resultHours.textContent = '8 часов';
-  } else if (+inputRange.value === 130) {
-    resultPersonal.textContent = '4 человека';
-    resultHours.textContent = '7 часов';
-  } else if (+inputRange.value === 140) {
-    resultPersonal.textContent = '4 человека';
-    resultHours.textContent = '7 часов';
-  } else if (+inputRange.value === 150) {
-    resultPersonal.textContent = '4 человека';
-    resultHours.textContent = '8 часов';
-  } else if (+inputRange.value === 160) {
-    resultPersonal.textContent = '4 человека';
-    resultHours.textContent = '8 часов';
-  } else if (+inputRange.value === 170) {
-    resultPersonal.textContent = '5 человек';
-    resultHours.textContent = '7 часов';
-  } else if (+inputRange.value >= 180 && +inputRange.value <= 200) {
-    resultPersonal.textContent = '5 человек';
-    resultHours.textContent = '8 часов';
+  for (let i = 0; i < resultPersonal.length && i < resultHours.length; i++) {
+    if (+inputRange.value === 10) {
+      resultPersonal[i].textContent = '1 человек';
+      resultHours[i].textContent = '2 часа';
+    } else if (+inputRange.value === 20) {
+      resultPersonal[i].textContent = '1 человек';
+      resultHours[i].textContent = '4 часа';
+    } else if (+inputRange.value === 30) {
+      resultPersonal[i].textContent = '1 человек';
+      resultHours[i].textContent = '6 часов';
+    } else if (+inputRange.value === 40) {
+      resultPersonal[i].textContent = '1 человек';
+      resultHours[i].textContent = '8 часов';
+    } else if (+inputRange.value === 50) {
+      resultPersonal[i].textContent = '2 человека';
+      resultHours[i].textContent = '5 часов';
+    } else if (+inputRange.value === 60) {
+      resultPersonal[i].textContent = '2 человека';
+      resultHours[i].textContent = '6 часов';
+    } else if (+inputRange.value === 70) {
+      resultPersonal[i].textContent = '2 человека';
+      resultHours[i].textContent = '7 часов';
+    } else if (+inputRange.value === 80) {
+      resultPersonal[i].textContent = '2 человека';
+      resultHours[i].textContent = '8 часов';
+    } else if (+inputRange.value === 90) {
+      resultPersonal[i].textContent = '3 человека';
+      resultHours[i].textContent = '6 часов';
+    } else if (+inputRange.value === 100) {
+      resultPersonal[i].textContent = '3 человека';
+      resultHours[i].textContent = '7 часов';
+    } else if (+inputRange.value === 110) {
+      resultPersonal[i].textContent = '3 человека';
+      resultHours[i].textContent = '8 часов';
+    } else if (+inputRange.value === 120) {
+      resultPersonal[i].textContent = '3 человека';
+      resultHours[i].textContent = '8 часов';
+    } else if (+inputRange.value === 130) {
+      resultPersonal[i].textContent = '4 человека';
+      resultHours[i].textContent = '7 часов';
+    } else if (+inputRange.value === 140) {
+      resultPersonal[i].textContent = '4 человека';
+      resultHours[i].textContent = '7 часов';
+    } else if (+inputRange.value === 150) {
+      resultPersonal[i].textContent = '4 человека';
+      resultHours[i].textContent = '8 часов';
+    } else if (+inputRange.value === 160) {
+      resultPersonal[i].textContent = '4 человека';
+      resultHours[i].textContent = '8 часов';
+    } else if (+inputRange.value === 170) {
+      resultPersonal[i].textContent = '5 человек';
+      resultHours[i].textContent = '7 часов';
+    } else if (+inputRange.value >= 180 && +inputRange.value <= 200) {
+      resultPersonal[i].textContent = '5 человек';
+      resultHours[i].textContent = '8 часов';
+    }
   }
 
   // Калькулятор
